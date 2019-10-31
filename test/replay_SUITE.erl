c48c21c3b7026237eb31e62f8b58c35c61f81c38
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-10-31 11:43:05 +0100

    Add replay test/sample

---
+++ test/replay_SUITE.erl
@@ -0,0 +1,68 @@
+%% Copyright (c) 2018 Pivotal Software Inc, All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%       https://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
+-module(replay_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-compile(export_all).
+
+all() ->
+    [
+        replay
+    ].
+
+group() -> [].
+
+%% -------------------------------------------------------------------
+%% Testsuite setup/teardown.
+%% -------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    application:load(ra),
+    WorkDirectory = proplists:get_value(priv_dir, Config),
+    ok = application:set_env(ra, data_dir, filename:join(WorkDirectory, "ra")),
+    ok = application:set_env(ra_kv_store, release_cursor_every, 1),
+    Config.
+
+end_per_suite(Config) ->
+    application:stop(ra),
+    Config.
+
+%% -------------------------------------------------------------------
+%% Testcases.
+%% -------------------------------------------------------------------
+
+replay(_Config) ->
+    Nodes = [{ra_kv1, node()}, {ra_kv2, node()}, {ra_kv3, node()}],
+    ClusterId = <<"ra_kv_store">>,
+    Config = #{},
+    Machine = {module, ra_kv_store, Config},
+    application:ensure_all_started(ra),
+    {ok, _, _} = ra:start_cluster(ClusterId, Machine, Nodes),
+    {ok, _} = ra_kv_store:write(ra_kv1, 1, 2),
+    {ok, {{read, 2}, _, _, _}} = ra_kv_store:cas(ra_kv1, 1, 2, 4),
+    {ok, {{read, 4}, _, _, _}} = ra_kv_store:cas(ra_kv1, 1, 3, 6),
+
+    {ok, {{read, undefined}, _, _, _}} = ra_kv_store:cas(ra_kv1, 2, undefined, 1),
+    {ok, {{read, 1}, _, _, _}} = ra_kv_store:cas(ra_kv1, 2, undefined, 3),
+
+    {ok, RaDataDir} = application:get_env(ra, data_dir),
+    WalFile = filename:join([RaDataDir, node(), "00000001.wal"]),
+
+    InitialState = ra_kv_store:init(Config),
+    FinalState = ra_dbg:replay_log(WalFile, ra_kv_store, InitialState),
+    FinalState = #{1 => 4, 2 => 1},
+    ok.
