b6fa646d5add0f0d85d2cc97be2ef9792d119cb9
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2021-11-02 10:17:29 +0100

    Back to Clojure 1.10.0
    Does not compile with higher version.

--- jepsen/jepsen.rakvstore/project.clj
+++ jepsen/jepsen.rakvstore/project.clj
@@ -8,5 +8,5 @@
   :license {:name "Apache 2.0 License"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :main jepsen.rakvstore
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [jepsen "0.1.19"]])
