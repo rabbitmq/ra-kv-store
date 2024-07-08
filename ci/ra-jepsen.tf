b453180d05befb5a0ee7e866011952898f8bebbd
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2024-07-08 15:43:38 +0200

    Use Debian 11

--- ci/ra-jepsen.tf
+++ ci/ra-jepsen.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "jepsen_controller" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = "debian-cloud/debian-11"
     }
   }
 
@@ -43,7 +43,7 @@ resource "google_compute_instance" "jepsen_worker" {
   machine_type = "n1-highcpu-2"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = "debian-cloud/debian-11"
     }
   }
 
