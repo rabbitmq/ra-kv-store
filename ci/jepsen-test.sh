3adb62fd4e7e8cbc113f781000a9e8c773fd1d92
Arnaud Cogoluègnes <acogoluegnes@tuta.io>
2024-12-03 09:16:31 +0100

    Add --quiet to S3 copy

--- ci/jepsen-test.sh
+++ ci/jepsen-test.sh
@@ -163,7 +163,7 @@ the_date=$(date '+%Y%m%d-%H%M%S')
 archive_name="ra-jepsen-$the_date-jepsen-logs"
 archive_file="$archive_name.tar.gz"
 ssh -o StrictHostKeyChecking=no -i jepsen-bot $JEPSEN_USER@$CONTROLLER_IP "$SOURCE_AND_CD ; tar -zcf - store --transform='s/^store/${archive_name}/'" > $archive_file
-aws s3 cp $archive_file s3://jepsen-tests-logs/
+aws s3 cp $archive_file s3://jepsen-tests-logs/ --quiet
 
 WORKER_INDEX=0
 for worker_ip in "${WORKERS_IP[@]}"
@@ -172,7 +172,7 @@ do
 	var_archive_file="$var_archive_name.tar.gz"
 	ssh -o StrictHostKeyChecking=no -i jepsen-bot $JEPSEN_USER@$worker_ip \
 		"cd /tmp ; tar -zcf - ra-kv-store-var --transform='s/^ra-kv-store-var/${var_archive_name}/'" > $var_archive_file
-  aws s3 cp $var_archive_file s3://jepsen-tests-logs/
+  aws s3 cp $var_archive_file s3://jepsen-tests-logs/ --quiet
 	((WORKER_INDEX++))
 done
 
