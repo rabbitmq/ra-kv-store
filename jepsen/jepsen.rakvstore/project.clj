e573255d891a141c67fa4f7586f27a4e493fe823
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2025-12-03 11:04:08 +0000

    Bump Jepsen to 0.3.10

--- jepsen/jepsen.rakvstore/project.clj
+++ jepsen/jepsen.rakvstore/project.clj
@@ -7,8 +7,8 @@
   :license {:name "Apache 2.0 License"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :main jepsen.rakvstore
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [jepsen "0.3.9"]]
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [jepsen "0.3.10"]]
   :exclusions [org.slf4j/log4j-over-slf4j
                log4j/log4j]
 
